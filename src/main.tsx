5b2e10e8c4ac5b4178f793246424753886c9cf96
Gabriel <gabrielmonte485@gmail.com>
2024-11-01 15:02:11 -0300

    Agora eu adicionai o Bootstrap e o React Router para poder facilitar as rotas, tirei algumas paginas desnecessarias.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+//import App from './App.tsx'
+import ErrorPage from "./routes/error-page.tsx";
+import Root from "./routes/root.tsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
